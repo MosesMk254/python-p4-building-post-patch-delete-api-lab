4fd77ac14a05f5080fbe034c589bf315e23e0c4a
Moses <mutisyamoses722@gmail.com>
2024-07-02 13:08:38 +0300

    crud lab

--- server/app.py
+++ server/app.py
@@ -23,12 +23,32 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
+        return make_response(jsonify(bakery_serialized), 200)
+    elif request.method == 'PATCH':
+        data = request.form
+        if 'name' in data:
+            bakery.name = data['name']
+            db.session.commit()
+            return make_response(jsonify(bakery.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Name not provided"}), 400)
+        
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    new_baked_good = BakedGood(
+        name=data.get('name'),
+        price=data.get('price'),
+        bakery_id=data.get('bakery_id')
+    )
+    db.session.add(new_baked_good)
+    db.session.commit()
+    return make_response(jsonify(new_baked_good.to_dict()), 201)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -44,6 +64,16 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    if baked_good:
+        db.session.delete(baked_good)
+        db.session.commit()
+        return make_response(jsonify({"message": "Baked good successfully deleted"}), 200)
+    else:
+        return make_response(jsonify({"error": "Baked good not found"}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
